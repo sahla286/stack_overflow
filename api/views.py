e5d29c51005f16c1cd38ec35110a6f60ff51d23e
sahla <tcsahla@gmail.com>
2024-09-24 21:19:19 +0530

    first commit

--- api/views.py
+++ api/views.py
@@ -3,6 +3,10 @@ from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth.models import User
 from api.serializer import UserSerializer,QuestionSerializer
 from .models import Questions
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -11,6 +15,22 @@ class UserView(ModelViewSet):
     queryset=User.objects.all()
 
 class QuestionsView(ModelViewSet):
-    serializer_class=QuestionSerializer
-    queryset=Questions.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = QuestionSerializer
+    queryset = Questions.objects.all()
     
+    def create(self, request, *args, **kwargs):
+        user=request.user
+        ser=QuestionSerializer(data=request.data)
+        if ser.is_valid():
+            ser.save(user=user)
+            return Response(data=ser.data, status=status.HTTP_201_CREATED)
+        return Response(data=ser.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def list(self, request, *args, **kwargs):
+        user=request.user
+        questions=self.queryset.filter(user=user)
+        serializer=self.serializer_class(questions,many=True)
+        return Response(data=serializer.data,status=status.HTTP_200_OK)
+
--- stack_overflow/urls.py
+++ stack_overflow/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from api import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import ObtainAuthToken 
 
 router=DefaultRouter()
 
 router.register('users',views.UserView,basename='users')
+router.register('question',views.QuestionsView,basename='question')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token',ObtainAuthToken.as_view()),
 ] + router.urls
